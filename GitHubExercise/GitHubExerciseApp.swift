375ee0a17072c1ed21d42a1f2b911f4d07f6becf
Nicoletta <nicoletta.cholaky@gmail.com>
2024-07-02 11:16:40 +0200

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Nicoletta Cholaky on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
